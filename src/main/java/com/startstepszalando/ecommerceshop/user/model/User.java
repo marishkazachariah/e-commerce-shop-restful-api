d6ddf36a3cd0bab82d7ab50e59474404b425ca33
marishkazachariah <marishka.zachariah@gmail.com>
2024-01-09 17:10:59 +0100

    PXMB-14 add validation to User attributes/fields as specified on ticket

--- src/main/java/com/startstepszalando/ecommerceshop/user/model/User.java
+++ src/main/java/com/startstepszalando/ecommerceshop/user/model/User.java
@@ -1,6 +1,9 @@
 package com.startstepszalando.ecommerceshop.user.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -23,8 +26,15 @@ public class User implements UserDetails {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank(message = "Name cannot be blank")
     private String name;
+
+    @NotBlank(message = "Email cannot be blank")
+    @Email(message = "Please provide a valid email address")
     private String email;
+
+    @NotBlank(message = "Password cannot be blank")
+    @Size(min = 8, message = "Password must be at least 8 characters long")
     private String password;
 
     @ElementCollection(targetClass = Role.class, fetch = FetchType.EAGER)
