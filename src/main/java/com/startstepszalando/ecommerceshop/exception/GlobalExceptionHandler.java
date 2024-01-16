fb15c275e2936ed5b90e915638c0c9ae9532cdc7
marishkazachariah <marishka.zachariah@gmail.com>
2024-01-16 10:20:56 +0100

    PXMB-25 add logger to GlobalExceptionHandler

--- src/main/java/com/startstepszalando/ecommerceshop/exception/GlobalExceptionHandler.java
+++ src/main/java/com/startstepszalando/ecommerceshop/exception/GlobalExceptionHandler.java
@@ -6,12 +6,16 @@ import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.HashMap;
 import java.util.Map;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
+    private final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
     @ExceptionHandler(UserNotFoundException.class)
     public ResponseEntity<?> handleUserNotFoundException(UserNotFoundException ex) {
         return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
@@ -30,6 +34,7 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(PasswordPolicyException.class)
     public ResponseEntity<?> handlePasswordPolicyException(PasswordPolicyException ex) {
+        logger.error(ex.toString());
         return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
     }
 
@@ -46,16 +51,20 @@ public class GlobalExceptionHandler {
             String errorMessage = error.getDefaultMessage();
             errors.put(fieldName, errorMessage);
         });
+        logger.error(errors.toString());
+
         return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(TokenValidationException.class)
     public ResponseEntity<?> handleTokenValidationException(TokenValidationException ex) {
+        logger.error(ex.toString());
         return new ResponseEntity<>(ex.getMessage(), HttpStatus.UNAUTHORIZED);
     }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<?> handleException(Exception ex) {
+        logger.error(ex.toString());
         return new ResponseEntity<>("An error occurred: " + ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
