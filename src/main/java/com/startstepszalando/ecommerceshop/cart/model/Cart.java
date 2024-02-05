062532447c5952581ba6edcbb459b3714d82c6b5
marishkazachariah <marishka.zachariah@gmail.com>
2024-02-05 15:12:19 +0100

    PXMB-40 create all models and classes required for the cart

---
+++ src/main/java/com/startstepszalando/ecommerceshop/cart/model/Cart.java
@@ -0,0 +1,26 @@
+package com.startstepszalando.ecommerceshop.cart.model;
+
+import com.startstepszalando.ecommerceshop.user.model.User;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "carts")
+public class Cart {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    private User user;
+
+    @OneToMany(mappedBy = "cart", cascade = CascadeType.ALL, orphanRemoval = true)
+    private Set<CartItem> items = new HashSet<>();
+}
